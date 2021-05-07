97d2232a1d252a1ff69270eb99fb10878c07b133
Alan Libby <>
2021-05-07 13:10:17 -0400

    Add comments in previous solution

--- 6 kyu/Can You OOP in C - Information Hiding 5a2b86ce1f7f7052b800006d/solution.c
+++ 6 kyu/Can You OOP in C - Information Hiding 5a2b86ce1f7f7052b800006d/solution.c
@@ -16,8 +16,12 @@ array_minmax_t *array_minmax_create(unsigned int length, int *input_array)
   array_minmax_t *out = (array_minmax_t *)malloc(sizeof(array_minmax_t));
   out->internal_length = length * 2;
   out->length = length;
+
+  // initialize the internal array with more space than needed so we don't have to reallocate every time we add to it
   out->arr = (int *)malloc(sizeof(int) * out->internal_length);
   memcpy(out->arr, input_array, sizeof(int) * length);
+
+  // calculate min/max from initial array
   out->min = input_array[0];
   out->max = input_array[0];
   for (unsigned int i = 1; i < length; ++i)
@@ -32,6 +36,7 @@ array_minmax_t *array_minmax_create(unsigned int length, int *input_array)
 
 void array_minmax_add(array_minmax_t *array, int number)
 {
+  // reallocate if needed
   if (array->length == array->internal_length)
   {
     array->internal_length *= 2;
@@ -40,13 +45,18 @@ void array_minmax_add(array_minmax_t *array, int number)
     free(array->arr);
     array->arr = new_arr;
   }
+
+  // now guaranteed an allocated and empty space at arr[length]
   array->arr[array->length++] = number;
+
+  // if number is new max or min, update those
   if (number < array->min)
     array->min = number;
   if (number > array->max)
     array->max = number;
 }
 
+// O(1) :D
 int array_minmax_get_min(array_minmax_t *array)
 {
   return array->min;
@@ -59,21 +69,23 @@ int array_minmax_get_max(array_minmax_t *array)
 
 void array_minmax_destroy(array_minmax_t *array)
 {
+  // we allocated array->arr so have to free it as well as the struct to prevent memory leaks
   free(array->arr);
   free(array);
 }
 
 // Test
+// Codewars runner uses Criterion testing framework; I implement cr_assert_eq myself to avoid the dependency
 void cr_assert_eq(int actual, int expected, const char *msg)
 {
   if (actual == expected)
     return;
-  printf("%s\nExpected: %d\nGot:     %d\n\n", msg, expected, actual);
+  printf("%s\nExpected: %d\nGot:      %d\n\n", msg, expected, actual);
 }
 
 int main()
 {
-  printf("%s\n\n", "Running given sample tests; no further output indicates success");
+  printf("Running given sample tests; no further output indicates success\n\n");
   static array_minmax_t *array;
   static int input_array[5] = {4, 2, 9, 7, 1};
 
